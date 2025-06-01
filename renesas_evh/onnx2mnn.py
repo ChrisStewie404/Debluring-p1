60e44e915958bfb34a4fbefc61e4e5194f583ffe
hzx <huang_zheng_xiang@163.com>
2025-06-01 15:14:02 +0800

    add and update renesas_evh

---
+++ renesas_evh/onnx2mnn.py
@@ -0,0 +1,24 @@
+import _tools as MNNTools
+
+def export_mnn2onnx(onnx_path,mnn_path):
+    convert_args = [
+        '',
+        '-f',
+        'ONNX',
+        '--modelFile',
+        onnx_path,
+        '--MNNModel',
+        mnn_path,
+        '--keepInputFormat',
+        '--bizCode',
+        'biz',
+    ]
+    MNNTools.mnnconvert(convert_args)
+
+if  __name__ == "__main__":
+    fpn_onnx_path = 'FPNInception_736_1312.onnx'
+    yolo_onnx_path = 'yolo11n.onnx'
+    fpn_mnn_path = 'FPNInception_736_1312.mnn'
+    yolo_mnn_path = 'yolo11n.mnn'
+    export_mnn2onnx(fpn_onnx_path,fpn_mnn_path)
+    export_mnn2onnx(yolo_onnx_path,yolo_mnn_path) 
